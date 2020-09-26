f8769947b131d0f5438cdd3be635b5ff60d72a4b
BENMARVYN <bskywalker76@outlook.com>
2020-09-26 17:47:42 +0300

    created user and cart sagas for sign in, sign up and sign in with google

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -19,6 +19,42 @@
 //     collections => (collections ? collections[collectionUrlParam] : null)
 //   );
 
+//   import { createSelector } from 'reselect';
+
+// const selectShop = state => state.shop;
+
+// export const selectCollections = createSelector(
+//   [selectShop],
+//   shop => shop.collections
+// );
+
+// export const selectCollectionsForPreview = createSelector(
+//   [selectCollections],
+//   collections =>
+//     collections ? Object.keys(collections).map(key => collections[key]) : []
+// );
+
+// export const selectCollection = collectionUrlParam =>
+//   createSelector(
+//     [selectCollections],
+//     collections => (collections ? collections[collectionUrlParam] : null)
+//   );
+
+// export const selectIsCollectionFetching = createSelector(
+//   [selectShop],
+//   shop => shop.isFetching
+// );
+
+// export const selectIsCollectionsLoaded = createSelector(
+//   [selectShop],
+//   shop => !!shop.collections
+// );
+
+
+
+
+
+
 import { createSelector } from 'reselect';
 
 const selectShop = state => state.shop;
@@ -39,3 +75,13 @@ export const selectCollection = collectionUrlParam =>
     [selectCollections],
     collections => (collections ? collections[collectionUrlParam] : null)
   );
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  shop => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+  [selectShop],
+  shop => !!shop.collections
+);
